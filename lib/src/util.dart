dd2a116a56827639d5dc626f3011ed3ba17ebe18
Aaron Madlon-Kay <aaron@madlon-kay.com>
2020-03-25 01:41:14 +0900

    Add IndentedRegionParser

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -82,3 +82,94 @@ class StartOfInputParser extends Parser<void> {
   bool hasEqualProperties(StartOfInputParser other) =>
       super.hasEqualProperties(other) && message == other.message;
 }
+
+const kUnlimitedSeparatingLineBreaks = -1;
+
+Parser indentedRegion(
+        {Parser parser,
+        int indentAdjust = 0,
+        int maxSeparatingLineBreaks = -1}) =>
+    IndentedRegionParser(parser, indentAdjust, maxSeparatingLineBreaks);
+
+class IndentedRegionParser extends DelegateParser {
+  IndentedRegionParser(
+      Parser delegate, this.indentAdjust, this.maxSeparatingLineBreaks)
+      : assert(maxSeparatingLineBreaks == kUnlimitedSeparatingLineBreaks ||
+            maxSeparatingLineBreaks > 0),
+        super(delegate ?? any().star().flatten('Region content expected'));
+
+  final int indentAdjust;
+  final int maxSeparatingLineBreaks;
+
+  @override
+  DelegateParser copy() =>
+      IndentedRegionParser(delegate, indentAdjust, maxSeparatingLineBreaks);
+
+  @override
+  Result parseOn(Context context) {
+    final end = _endOfRegion(context.buffer, context.position);
+    final delegateResult = delegate.parseOn(_regionContext(context, end));
+    if (delegateResult.isFailure) {
+      return context.failure('Indented region delegate parser failed');
+    } else {
+      return context.success(delegateResult.value, end);
+    }
+  }
+
+  Context _regionContext(Context current, int regionEnd) {
+    if (current.buffer.length == regionEnd) {
+      return current;
+    } else {
+      final regionBuffer =
+          current.buffer.substring(current.position, regionEnd);
+      return Context(regionBuffer, 0);
+    }
+  }
+
+  @override
+  int fastParseOn(String buffer, int position) {
+    final end = _endOfRegion(buffer, position);
+    final delegateResult =
+        delegate.fastParseOn(buffer.substring(position, end), 0);
+    return delegateResult >= 0 ? end : -1;
+  }
+
+  int _endOfRegion(String buffer, int position) {
+    final startOfLine = buffer.lastIndexOf('\n', position);
+    final indent = (startOfLine < 0 ? position : position - startOfLine - 1) +
+        indentAdjust;
+    final indentPattern = ' ' * indent;
+    var here = _endOfNextNewLineRun(buffer, position);
+    while (here >= 0 && here < buffer.length - indent) {
+      if (!buffer.startsWith(indentPattern, here)) {
+        break;
+      }
+      here = _endOfNextNewLineRun(buffer, here);
+    }
+    if (here < 0) {
+      here = buffer.length;
+    }
+    assert(here >= 0, 'Region detection should never fail');
+    return here;
+  }
+
+  int _endOfNextNewLineRun(String buffer, int position) {
+    final start = buffer.indexOf('\n', position);
+    var here = start;
+    var lines = 0;
+    while (
+        here >= 0 && here < buffer.length && buffer.codeUnitAt(here) == 0x0a) {
+      final next = here + 1;
+      if (next < buffer.length && buffer.codeUnitAt(next) == 0x0d) {
+        here++;
+      }
+      lines++;
+      if (maxSeparatingLineBreaks != kUnlimitedSeparatingLineBreaks &&
+          lines > maxSeparatingLineBreaks) {
+        break;
+      }
+      here++;
+    }
+    return here;
+  }
+}
---
+++ test/util_test.dart
@@ -0,0 +1,77 @@
+import 'package:org_parser/src/util.dart';
+import 'package:petitparser/petitparser.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('indent parser', () {
+    final parser = whitespace().star() & indentedRegion();
+    var result = parser.end().pick(1).parse('''  abc
+  def
+  hij''');
+    expect(result.value, 'abc\n  def\n  hij');
+    result = parser.pick(1).parse('''  abc
+  def
+ hij''');
+    expect(result.value, 'abc\n  def\n',
+        reason: 'last line has less indentation');
+    result = parser.end().pick(1).parse('''  abc
+   def
+  hij''');
+    expect(result.value, 'abc\n   def\n  hij',
+        reason: 'more indentation is ok');
+    result = parser.end().pick(1).parse('''  abc
+  def
+
+  hij''');
+    expect(result.value, 'abc\n  def\n\n  hij', reason: 'blank lines are ok');
+  });
+  test('indent parser with max blank lines', () {
+    final parser =
+        whitespace().star() & indentedRegion(maxSeparatingLineBreaks: 1);
+    final result = parser.pick(1).parse('''  abc
+  def
+
+  hij''');
+    expect(result.value, 'abc\n  def\n', reason: 'blank lines are ok');
+  });
+  test('indent parser with adjustment', () {
+    final parser = whitespace().star() & indentedRegion(indentAdjust: 1);
+    var result = parser.end().pick(1).parse('''  abc
+   def
+   hij''');
+    expect(result.value, 'abc\n   def\n   hij');
+    result = parser.pick(1).parse('''  abc
+ def
+ hij''');
+    expect(result.value, 'abc\n',
+        reason: '1 extra space of indentation required');
+  });
+  test('recursive list parser', () {
+    final listStart =
+        (lineStart() & whitespace().star() & string('- ')).flatten();
+    final list = undefined();
+    list.set(listStart &
+        indentedRegion(
+            parser: (list | any().plusLazy(listStart | endOfInput()).flatten())
+                .star()));
+    final result = list.plus().end().parse('''- abc
+- def
+  - hij''');
+    expect(result.value, [
+      [
+        '- ',
+        ['abc\n']
+      ],
+      [
+        '- ',
+        [
+          'def\n',
+          [
+            '  - ',
+            ['hij']
+          ]
+        ]
+      ]
+    ]);
+  });
+}
