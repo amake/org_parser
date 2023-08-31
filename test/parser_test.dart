d885bfdb69750555ea0912dbd61f7c078adfc59c
Aaron Madlon-Kay <aaron@madlon-kay.com>
2023-08-31 20:57:00 +0900

    Add list parser tests

--- test/parser_test.dart
+++ test/parser_test.dart
@@ -200,6 +200,78 @@ bar/''');
         expect(row0.cells[0].content.children.isEmpty, true);
       });
     });
+    group('list', () {
+      final parser = buildSpecific(parserDefinition.list);
+      test('single line', () {
+        final result = parser.parse('- foo');
+        final list = result.value as OrgList;
+        expect(list.items.length, 1);
+        final body = list.items[0].body?.children[0] as OrgPlainText;
+        expect(body.content, 'foo');
+      });
+      test('multiple lines', () {
+        final result = parser.parse('''- foo
+  - bar''');
+        final list = result.value as OrgList;
+        expect(list.items.length, 1);
+        final sublist = list.items[0].body?.children[1] as OrgList;
+        final body = sublist.items[0].body?.children[0] as OrgPlainText;
+        expect(body.content, 'bar');
+      });
+      test('multiline item', () {
+        final result = parser.parse('''- foo
+
+  bar''');
+        final list = result.value as OrgList;
+        expect(list.items.length, 1);
+        final body = list.items[0].body?.children[0] as OrgPlainText;
+        expect(body.content, 'foo\n\n  bar');
+      });
+      test('multiline item with eol white space', () {
+        final result = parser.parse(
+          '  - foo\n'
+          ' \n'
+          '    bar',
+        );
+        final list = result.value as OrgList;
+        expect(list.items.length, 1);
+        final body = list.items[0].body?.children[0] as OrgPlainText;
+        expect(body.content, 'foo\n \n    bar');
+      });
+      test('complex', () {
+        final result = parser.parse('''30. [@30] foo
+   - bar :: baz
+     blah
+   - [ ] *bazinga*''');
+        final list = result.value as OrgList;
+        final item0 = list.items[0] as OrgListOrderedItem;
+        expect(item0.bullet, '30. ');
+        expect(item0.checkbox, isNull);
+        expect(item0.counterSet, '[@30]');
+        final sublist = list.items[0].body?.children[1] as OrgList;
+        final item1 = sublist.items[0] as OrgListUnorderedItem;
+        expect(item1.bullet, '- ');
+        expect(item1.checkbox, isNull);
+        expect(item1.tagDelimiter, ' :: ');
+      });
+      test('item with block', () {
+        final result = parser.parse('''- foo
+  #+begin_src sh
+    echo bar
+  #+end_src''');
+        final list = result.value as OrgList;
+        final block = list.items[0].body?.children[1] as OrgBlock;
+        expect(block.header, '#+begin_src sh\n');
+      });
+      test('with tag', () {
+        final result = parser.parse('- ~foo~ ::');
+        final list = result.value as OrgList;
+        final item = list.items[0] as OrgListUnorderedItem;
+        expect(item.tagDelimiter, ' ::');
+        final markup = item.tag?.children[0] as OrgMarkup;
+        expect(markup.content, 'foo');
+      });
+    });
     test('planning line', () {
       final parser = buildSpecific(parserDefinition.planningLine);
       final result = parser.parse(
