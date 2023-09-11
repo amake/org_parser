17a809b8a0c5ebda6a2d9bb6f1ab1ccf936223e1
Aaron Madlon-Kay <aaron@madlon-kay.com>
2023-09-11 21:24:48 +0900

    Use scopes to organize example.dart

--- example/example.dart
+++ example/example.dart
@@ -1,21 +1,23 @@
 import 'package:org_parser/org_parser.dart';
 
 void main() {
-  // Parse a very simple document
-  const docString = '''* TODO [#A] foo bar
+  {
+    // Parse a very simple document
+    const docString = '''* TODO [#A] foo bar
 baz buzz''';
-  final doc = OrgDocument.parse(docString);
-  final section = doc.sections[0];
-  print(section.headline.keyword?.value);
-  final title = section.headline.title!.children[0] as OrgPlainText;
-  print(title.content);
-  final paragraph = section.content!.children[0] as OrgParagraph;
-  final body = paragraph.body.children[0] as OrgPlainText;
-  print(body.content);
+    final doc = OrgDocument.parse(docString);
+    final section = doc.sections[0];
+    print(section.headline.keyword?.value);
+    final title = section.headline.title!.children[0] as OrgPlainText;
+    print(title.content);
+    final paragraph = section.content!.children[0] as OrgParagraph;
+    final body = paragraph.body.children[0] as OrgPlainText;
+    print(body.content);
+  }
 
-  // Extract TODOs from a document
-
-  const agendaDoc = '''* TODO Go fishing
+  {
+    // Extract TODOs from a document
+    const docString = '''* TODO Go fishing
 ** Equipment
 - Fishing rod
 - Bait
@@ -25,15 +27,16 @@ baz buzz''';
 - Famous Ray's
 - Original Ray's
 * TODO Take a nap''';
-  final agenda = OrgDocument.parse(agendaDoc);
-  agenda.visitSections((section) {
-    if (section.headline.keyword?.value == 'TODO') {
-      final title = section.headline.title!.children
-          .whereType<OrgPlainText>()
-          .map((plainText) => plainText.content)
-          .join();
-      print("I'm going to ${title.toLowerCase()}");
-    }
-    return true;
-  });
+    final doc = OrgDocument.parse(docString);
+    doc.visitSections((section) {
+      if (section.headline.keyword?.value == 'TODO') {
+        final title = section.headline.title!.children
+            .whereType<OrgPlainText>()
+            .map((plainText) => plainText.content)
+            .join();
+        print("I'm going to ${title.toLowerCase()}");
+      }
+      return true;
+    });
+  }
 }
