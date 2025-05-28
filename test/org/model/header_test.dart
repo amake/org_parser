50e75689b8106644a071ecb5603a90d0d4ac07a3
Aaron Madlon-Kay <aaron@madlon-kay.com>
2025-05-29 07:12:54 +0900

    Group related tests

--- test/org/model/header_test.dart
+++ test/org/model/header_test.dart
@@ -31,79 +31,81 @@ void main() {
       expect(headline.tags!.values, ['あ']);
       expect(headline.toMarkup(), markup);
     });
-    test('test tag inheritance - children have no tags', () {
-      final markup = """
+    group('tag inheritance', () {
+      test('children have no tags', () {
+        final markup = """
 * School exams :school:
 ** some exam
 ** another exam""";
-      final result = OrgDocument.parse(markup);
-      final parentSection = result.sections[0];
-      final firstChildSection = parentSection.sections[0];
-      final secondChildSection = parentSection.sections[1];
-      expect(parentSection.tagsWithInheritance(result), ["school"]);
-      expect(parentSection.tags, ["school"]);
-      expect(firstChildSection.tagsWithInheritance(result), ["school"]);
-      expect(firstChildSection.tags, isEmpty);
-      expect(secondChildSection.tagsWithInheritance(result), ["school"]);
-      expect(secondChildSection.tags, isEmpty);
-      expect(parentSection.toMarkup(), markup);
-    });
-    test('test tag inheritance - parent and child have tags', () {
-      final markup = """
+        final result = OrgDocument.parse(markup);
+        final parentSection = result.sections[0];
+        final firstChildSection = parentSection.sections[0];
+        final secondChildSection = parentSection.sections[1];
+        expect(parentSection.tagsWithInheritance(result), ["school"]);
+        expect(parentSection.tags, ["school"]);
+        expect(firstChildSection.tagsWithInheritance(result), ["school"]);
+        expect(firstChildSection.tags, isEmpty);
+        expect(secondChildSection.tagsWithInheritance(result), ["school"]);
+        expect(secondChildSection.tags, isEmpty);
+        expect(parentSection.toMarkup(), markup);
+      });
+      test('parent and child have tags', () {
+        final markup = """
 * School exams :school:
 ** History exam :history:""";
-      final result = OrgDocument.parse(markup);
-      final parentSection = result.sections[0];
-      final childSection = parentSection.sections[0];
-      expect(parentSection.tagsWithInheritance(result), ["school"]);
-      expect(parentSection.tags, ["school"]);
-      expect(childSection.tagsWithInheritance(result), ["school", "history"]);
-      expect(childSection.tags, ["history"]);
-      expect(parentSection.toMarkup(), markup);
-    });
-    test('test tag inheritance - no tags', () {
-      final markup = """
+        final result = OrgDocument.parse(markup);
+        final parentSection = result.sections[0];
+        final childSection = parentSection.sections[0];
+        expect(parentSection.tagsWithInheritance(result), ["school"]);
+        expect(parentSection.tags, ["school"]);
+        expect(childSection.tagsWithInheritance(result), ["school", "history"]);
+        expect(childSection.tags, ["history"]);
+        expect(parentSection.toMarkup(), markup);
+      });
+      test('no tags', () {
+        final markup = """
 * School exams
 ** History exam""";
-      final result = OrgDocument.parse(markup);
-      final parentSection = result.sections[0];
-      final childSection = parentSection.sections[0];
-      expect(parentSection.tagsWithInheritance(result), isEmpty);
-      expect(parentSection.tags, isEmpty);
-      expect(childSection.tagsWithInheritance(result), isEmpty);
-      expect(childSection.tags, isEmpty);
-      expect(parentSection.toMarkup(), markup);
-    });
-    test('test tag inheritance - only child tags', () {
-      final markup = """
+        final result = OrgDocument.parse(markup);
+        final parentSection = result.sections[0];
+        final childSection = parentSection.sections[0];
+        expect(parentSection.tagsWithInheritance(result), isEmpty);
+        expect(parentSection.tags, isEmpty);
+        expect(childSection.tagsWithInheritance(result), isEmpty);
+        expect(childSection.tags, isEmpty);
+        expect(parentSection.toMarkup(), markup);
+      });
+      test('only child tags', () {
+        final markup = """
 * School exams
 ** History exam :history:""";
-      final result = OrgDocument.parse(markup);
-      final parentSection = result.sections[0];
-      final childSection = parentSection.sections[0];
-      expect(parentSection.tagsWithInheritance(result), isEmpty);
-      expect(parentSection.tags, isEmpty);
-      expect(childSection.tagsWithInheritance(result), ["history"]);
-      expect(childSection.tags, ["history"]);
-      expect(parentSection.toMarkup(), markup);
-    });
-    test('test tag inheritance - multi level inheritance', () {
-      final markup = """
+        final result = OrgDocument.parse(markup);
+        final parentSection = result.sections[0];
+        final childSection = parentSection.sections[0];
+        expect(parentSection.tagsWithInheritance(result), isEmpty);
+        expect(parentSection.tags, isEmpty);
+        expect(childSection.tagsWithInheritance(result), ["history"]);
+        expect(childSection.tags, ["history"]);
+        expect(parentSection.toMarkup(), markup);
+      });
+      test('multi level inheritance', () {
+        final markup = """
 * Heading A :tag1:
 **  Heading B :tag2:
 *** Heading C :tag3:""";
-      final result = OrgDocument.parse(markup);
-      final firstSection = result.sections[0];
-      final secondSection = firstSection.sections[0];
-      final thirdSection = secondSection.sections[0];
-      expect(firstSection.tagsWithInheritance(result), ["tag1"]);
-      expect(firstSection.tags, ["tag1"]);
-      expect(secondSection.tagsWithInheritance(result), ["tag1", "tag2"]);
-      expect(secondSection.tags, ["tag2"]);
-      expect(
-          thirdSection.tagsWithInheritance(result), ["tag1", "tag2", "tag3"]);
-      expect(thirdSection.tags, ["tag3"]);
-      expect(firstSection.toMarkup(), markup);
+        final result = OrgDocument.parse(markup);
+        final firstSection = result.sections[0];
+        final secondSection = firstSection.sections[0];
+        final thirdSection = secondSection.sections[0];
+        expect(firstSection.tagsWithInheritance(result), ["tag1"]);
+        expect(firstSection.tags, ["tag1"]);
+        expect(secondSection.tagsWithInheritance(result), ["tag1", "tag2"]);
+        expect(secondSection.tags, ["tag2"]);
+        expect(
+            thirdSection.tagsWithInheritance(result), ["tag1", "tag2", "tag3"]);
+        expect(thirdSection.tags, ["tag3"]);
+        expect(firstSection.toMarkup(), markup);
+      });
     });
   });
 }
